35227103d4da51e1ae8021ddc96c11ca5d9076b3
s66829020@gmail.com <s66829020@gmail.com>
2023-08-26 21:48:15 +0530

    first commit

---
+++ PageDirective/PageDirective/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PageDirective._Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container">
+        <br />
+        <h4>List Of Students</h4>
+       
+    </div>
+
+</asp:Content>
+
---
+++ PageDirective/PageDirective/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+
+namespace PageDirective
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataTable table = new DataTable();
+                table.Columns.Add("Name", typeof(string));
+                table.Columns.Add("Class", typeof(string));
+
+                table.Rows.Add("Walter", "6B");
+                table.Rows.Add("Edith", "6B");
+                table.Rows.Add("William", "6B");
+                table.Rows.Add("June", "6B");
+
+                foreach (DataRow dr in table.Rows)
+                {
+                    Response.Write(dr["Name"] + " is in class " + dr["Class"] + "<br>");
+                }
+            }
+        }
+    }
+}
